5d6f7e086d201ec40b4244fa39b7a4194c79fbe1
Atish Kathpal <atish.kathpal@gmail.com>
2016-09-30 11:30:36 +0530

    some more code

---
+++ breakup.py
@@ -0,0 +1,11 @@
+#https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/breakup-app/
+def getCounts(gender, line):
+
+if __name__ == "__main__":
+    N = int(raw_input())
+    count = []
+
+    while(N):
+        line = raw_input()
+        arr = line.split(":")
+        getCounts(arr[0], line)
